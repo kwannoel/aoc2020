fc37e15972022a55ee755bb2f3f0271dff2cc6b2
Kwan Noel <noelkwan1998@gmail.com>
2020-12-01 23:21:02 +0800

    Add day 1 shortened

---
+++ day-1-golfed.hs
@@ -0,0 +1,27 @@
+#!/usr/bin/env runghc
+
+{-
+
+---------- PROBLEM
+
+Given a list of numbers: find 2 numbers that add up to 2020, return their product
+
+---------- SOLUTION
+
+Hash numbers, perform lookup & shortcircuit after finding the first match
+
+-}
+
+import           Control.Arrow ((>>>))
+import           Data.Either   (fromRight)
+import           Data.Set      (empty, insert, member)
+
+main :: IO ()
+main = interact
+     $ fmap read . words
+   >>> fromRight (error "No matches") . foldr f (Left empty)
+   >>> show
+   where
+       f i (Left s) | (2020 - i) `member` s = Right $ i * (2020 - i)
+                    | otherwise = Left $ i `insert` s
+       f _ v = v
